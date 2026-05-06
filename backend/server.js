5e545f3fe96babf5acf5d34ed4b3827c30816dd8
---ZAIN--- <Zainhallak@koraaledu.nl>
2026-05-06 11:06:20 +0000

    fixes port

--- backend/server.js
+++ backend/server.js
@@ -113,6 +113,8 @@ app.get('/', (req, res) => {
   res.send('Backend running ✅');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
 });
