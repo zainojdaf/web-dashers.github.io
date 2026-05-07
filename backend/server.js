8e6a66c45aa644caa6610fdc1c5dd126460efb56
---ZAIN--- <Zainhallak@koraaledu.nl>
2026-05-07 14:26:55 +0200

    Remove dynamic import for node-fetch

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
